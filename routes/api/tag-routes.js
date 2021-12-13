dbada595c9184504edcb45d97bb37ff4103d6a39
Lyss Garcia <alyssa.g200@gmail.com>
2021-12-12 22:11:06 -0800

    all tag routes working!

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,8 @@ router.get('/', async(req, res) => {
     // be sure to include its associated Product data
     try {
         const tagData = await Tag.findAll({
-            include: [{ model: Product }]
+            attributes: ['id', 'tag_name'],
+            include: [{ model: Product, through: ProductTag, as: 'tag_toProducts' }]
         })
         res.status(200).json(tagData)
     } catch (err) {
@@ -21,7 +22,12 @@ router.get('/:id', async(req, res) => {
     // be sure to include its associated Product data
     try {
         const tagData = await Tag.findByPk(req.params.id, {
-            include: [{ model: Product }]
+            attributes: ['id', 'tag_name'],
+            include: [{
+                model: Product,
+                through: ProductTag,
+                as: 'tag_toProducts'
+            }]
         })
         if (!tagData) {
             res.status(404).json({ message: 'No Tag found with that id!' });
